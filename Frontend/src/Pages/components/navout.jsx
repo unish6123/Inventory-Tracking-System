50a3875898bb760a301c29b3cf629592e5d1d65c
S672672 <bsmith672@rku.ac.in>
2024-07-02 08:57:29 +0530

    navbar created

---
+++ Frontend/src/Pages/components/navout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navout() {
+  return (
+    <div>
+    <div className='w-full h-5 border-gray-300 items-center justify-center'>
+    <div className='text-black font-bold'>smartmart</div>
+    <div className='text'></div>
+    <div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navout
---
+++ Frontend/src/Pages/signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navout from './components/navout'
+
+function Signup() {
+  return (
+    <div className='h-full w-full'>
+    <Navout />
+    </div>
+  )
+}
+
+export default Signup
