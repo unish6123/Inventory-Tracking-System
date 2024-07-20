847ca44763d5091bcb3ee2cb82ff71ea3304d5e7
S672672 <bsmith672@rku.ac.in>
2024-07-20 21:16:04 +0530

    a small progress

--- Frontend/src/Pages/InsideWeeklyad.jsx
+++ Frontend/src/Pages/InsideWeeklyad.jsx
@@ -5,7 +5,6 @@ import Grilling from './components/Grilling'
 export default function InsideWeeklyad() {
   return (
     <div className='h-2/3 2-96'>
-    <InWeekly desc="let's get the weekly notice of what you want" image='./src/assets/pictures/momo.jpeg'/>
     <Grilling />
     </div>
   )
